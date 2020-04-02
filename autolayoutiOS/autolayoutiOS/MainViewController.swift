0aa91f84b5bf4da8ba0b6408b5debf25d8294bd0
Fabrício <guilhermo.fabricio@gmail.com>
2020-04-02 14:32:00 -0300

    Initial commit

---
+++ autolayoutiOS/autolayoutiOS/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  autolayoutiOS
+//
+//  Created by Fabrício Guilhermo on 02/04/20.
+//  Copyright © 2020 Fabrício Guilhermo. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+    }
+}
